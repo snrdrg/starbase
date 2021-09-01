d8b53e8b2de0372665e97271537b93b799e28d23
snrdrg <highcomunity@gmail.com>
2021-09-01 12:11:59 +0300

    Update AES.lua

--- AES.lua
+++ AES.lua
@@ -1,7 +1,7 @@
 goto (:AES+1)
 :fcuupdown=((:AESB<1000)or(:AESLB<1000)or(:AESRB<1000))*100
 :fcuupdown-=((:AEST<1000)or(:AESLT<1000)or(:AESRT<1000))*100
-:fcurightleft=((:AESRT<1000)or(:AESRM<1000)or(:AESRB<1000))*100
-:fcurightleft-=((:AESLT<1000)or(:AESLM<1000)or(:AESLB<1000))*100
-:fcurightleft=(:AESFR<1000)*100 :fcurightleft=(-1)*(:AESFL<1000)*100
+:fcurightleft=-((:AESRT<1000)or(:AESRM<1000)or(:AESRB<1000))*100
+:fcurightleft=((:AESLT<1000)or(:AESLM<1000)or(:AESLB<1000))*100
+:fcurightleft=(-1)*(:AESFR<1000)*100 :fcurightleft=(:AESFL<1000)*100
 goto 1
