9984864ca8e8ab8d0fd4d5994db1ddc320257aa8
snrdrg <highcomunity@gmail.com>
2021-09-01 11:47:30 +0300

    Update pointer.lua

--- pointer.lua
+++ pointer.lua
@@ -1,27 +1,22 @@
--- For use you need ISAN, advanced yolo chip, yolo memory chip, 24x24 text panel
--- allow external modules for ISAN
--- Name fields in memory chip as following :cx,:cy,:cz,:tx,:ty,:tz,:px,:py,:pz
--- Rename text panel as "Dir"
--- Name ISAN coords at memory chip - :cx,:cy,:cz
--- Target coords from memory chip - :tx,:ty,:tz - input manually
--- Prev tick ISAN coords, cached in memory chip :px,:py,:pz
----------- START YOLO CODE
-c1=0 c2=0 s="" 
-l=(:tx^2+:ty^2+:tz^2)^0.5
-nx=:tx/l ny=:ty/l nz=:tz/l 
-dx=:cx-:px dy=:cy-:py dz=:cz-:pz 
-dm=(dx^2+dy^2+dz^2)^0.5
-ndx=dx/dm dny=dy/dm ndz=dz/dm
-p=ndx*nx+ndy*ny+ndz*nz
-nx1=nx-p*ndx ny1=ny-p*ndy nz1=nz-p*ndz
-if p>0 then fwd="o" else fwd="x" end
-txx=((ny1/10000)*10000)*24
-txy=((nx1/10000)*10000)*24
-if c1<txy-1 then s+="\n" c+=1 goto 10 end
-if c2<txx-1 then s+=" " c+=1 goto 11 end
-s=s+fwd+"\n"
-:dir=s
--- delete this comment. Need to add check if moving from fcu
-:px=:cx :py=:cy :pz=:cz
-goto 1
----------- END OF YOLO CODE
+--ISAN heading and distance to target
+--Setup - 2 memory chips with following variables xx,yy,zz,tx,ty,tz,mx,my,mz,td,t_angl,px,py,pz
+--last version of ISAN (better Quad), with enabled modules (4 nav recievers, advanced yolo  chip)
+
+--basic yolo chip  with following  code
+if :px!=:xx or :py!=:yy or :pz!=:zz then goto 2 else goto 1 end
+:mx=:xx-:px :my=:yy-:py :mz=:zz-:pz
+--insert  delay 10 lines
+:px=:xx :py=:yy :pz=:zz goto 1
+
+--advanced yolo chip with following  code
+if (:px==:xx)and(:py==:yy)and(:pz==:zz) then goto 2 else goto 3 end
+:Heading="Not moving" goto 1
+tdx=:tx-:xx tdy=:ty-:yy tdz=:tz-:zz l=sqrt(tdx^2+tdy^2+tdz^2)
+if l==0 then :Heading="Arrived\n" goto1 end
+ct=(tdx*:mx+tdy*:my+tdz*:mz)/(sqrt((:mx)^2+(:my)^2+(:mz)^2)*l)
+:t_angl=180*sqrt((1-ct)/(4+ct)) :td=(l/100000)*100
+:Heading="Trgt:\n"+:t_angl+" deg\n"+:td+"km" goto 1
+
+--24x24 text panel with name "Heading"
+
+--fields tx,ty,tz - coordinates of target point
