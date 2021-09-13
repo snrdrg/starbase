a5e011a7283237d04af46bedf97fdecda7f0c7f2
snrdrg <highcomunity@gmail.com>
2021-09-13 12:28:12 +0300

    Update heading.lua

--- heading.lua
+++ heading.lua
@@ -3,15 +3,15 @@
 --last version of ISAN (better Quad), with enabled modules (4 nav recievers, advanced yolo  chip)
 
 --basic yolo chip  with following  code
-if :px!=:xx or :py!=:yy or :pz!=:zz then goto 2 else goto 1 end
-:mx=:xx-:px :my=:yy-:py :mz=:zz-:pz
+if :px!=:x or :py!=:y or :pz!=:z then goto 2 else goto 1 end
+:mx=:x-:px :my=:y-:py :mz=:z-:pz
 --insert  delay 10 lines
-:px=:xx :py=:yy :pz=:zz goto 1
+:px=:x :py=:y :pz=:z goto 1
 
 --advanced yolo chip with following  code
 if (:px==:xx)and(:py==:yy)and(:pz==:zz) then goto 2 else goto 3 end
 :Heading="Not moving" goto 1
-tdx=:tx-:xx tdy=:ty-:yy tdz=:tz-:zz l=sqrt(tdx^2+tdy^2+tdz^2)
+tdx=:tx-:x tdy=:ty-:y tdz=:tz-:z l=sqrt(tdx^2+tdy^2+tdz^2)
 if l==0 then :Heading="Arrived\n" goto1 end
 ct=(tdx*:mx+tdy*:my+tdz*:mz)/(sqrt((:mx)^2+(:my)^2+(:mz)^2)*l)
 :t_angl=180*sqrt((1-ct)/(4+ct)) :td=(l/100000)*100
